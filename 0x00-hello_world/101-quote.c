3c9f0c7391aa1a78c1871304fae34377a6b43470
Tajudeen <modinattajudeen776@gmail.com>
2022-12-08 20:42:30 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio>
+
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (success)
+ */
+int main (void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 61);
+
+	return (1);
+}
