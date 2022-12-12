e569011e958e64ac284c659a727389cf9f12e1dc
Tajudeen <modinattajudeen776@gmail.com>
2022-12-12 13:06:56 -0800

    srcond c task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include (stdlib.h)
+#include (time.h)
+#include (stdio)
 /**
- * main - block
- *
+ * main - main block
+ * Description: get a random number and print the number
+ * and if it is positive, negative or zero
  * Return: 0
  */
 int main(void)
@@ -11,15 +12,11 @@ int main(void)
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n < 0)
-	{
-		printf("%d is positive\n", n);
-	}	else if (n == 0)
-	{
-		printf("%d is zero"\n, n);
-	}	else
-	{
-		printf("%d is negative\n", n);
-	}
+	if (n > 0)
+		printf("%i is posotive\n", n);
+	else if (n < 0)
+		printf("%i is negative\n", n);
+	else 
+		printf("%i is zero\n", n);
 	Return (0);
 }
